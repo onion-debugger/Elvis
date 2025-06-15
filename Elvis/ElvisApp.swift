6f3c8d68dfdf09df1bb3b32a71e1a8d12e790140
onion_debugger <oshiole94@gmail.com>
2025-06-15 01:22:22 +0100

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by onion_debugger on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
